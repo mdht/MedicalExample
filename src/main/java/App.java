18e6c59f89b0ccd32df76a30270b32060840d187
Sean Muir <sean.muir@jkmsoftware.com>
2020-04-23 18:12:15 -0400

    Updates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,51 @@
-import org.openehealth.ipf.modules.cda.CDAR2Renderer;
-import org.openhealthtools.mdht.uml.cda.ClinicalDocument;
-import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
-
 import java.io.FileInputStream;
 
-public class App {
+import org.eclipse.mdht.uml.cda.ClinicalDocument;
+import org.eclipse.mdht.uml.cda.IdentityDoc;
+import org.eclipse.mdht.uml.cda.Participant1;
+import org.eclipse.mdht.uml.cda.util.CDAUtil;
+
+ public class App {
 
     public static void main(String[] args) throws Throwable {
-        FileInputStream file = new FileInputStream("/Users/Admin/Desktop/minzdrav/SampleCDADocumentRuMedSertOfBirth.xml");
+        FileInputStream file = new FileInputStream("src/test/resources/SampleCDADocumentRuMedSertOfBirth.xml");
         //unmarshalling
         ClinicalDocument cda = CDAUtil.load(file);
 
         //marshalling
-        CDAR2Renderer renderer = new CDAR2Renderer();
-        System.out.println(renderer.render(cda));
+    	for (Participant1 p1 : cda.getParticipants()) {
+			System.out.println(p1.getAssociatedEntity().getIds().get(0).getRoot());
+
+			p1.getAssociatedEntity().getIds().get(0).setRoot("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+			for (IdentityDoc doc : p1.getAssociatedEntity().getIdentityDocs()) {
+				System.out.println(doc.getSeries().getText());
+
+				System.out.println(doc.getIssueDate().getValue());
+
+				// System.out.println(doc.getIdentityCardTypeId().getCode());
+
+				// System.out.println(doc.getIssueDate());
+
+				// System.out.println(doc.getIssueOrgCode().getText());
+
+				// System.out.println(doc.getIssueOrgName().getText());
+				// System.out.println("get getSeries");
+				// System.out.println(doc.getSeries().getText());
+				// System.out.println("get getIssueOrgCode");
+				// System.out.println(doc.getIssueOrgCode().getText());
+				// doc.setSeries(DatatypesFactory.eINSTANCE.createST("hererere"));
+				// System.out.println(doc.getSeries().getText());
+
+			}
+
+		}
+
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaa");
+		 
+
+	 
+//        CDAR2Renderer renderer = new CDAR2Renderer();
+      CDAUtil.save(cda, System.out);
     }
 
 }
